5e1b11e95ca3eec92f0a27d0fdbe490c2b2d4a00
Ermolaev Andrey <enonesohc@yandex.ru>
2024-03-04 21:41:50 +0300

    123

--- test_page_object/pages/base_page.py
+++ test_page_object/pages/base_page.py
@@ -1,10 +1,10 @@
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
+
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-
 
     def find_element_with_wait(self, locator):
         (WebDriverWait(self.driver, 5).
@@ -13,23 +13,18 @@ class BasePage:
 
     def click_on_element(self, locator):
         element = self.find_element_with_wait(locator)
-        self.driver.execute_script("arguments[0].scrollIntoView();", element)
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         element.click()
 
     def get_text_from_element(self, locator):
         element = self.find_element_with_wait(locator)
-        self.driver.execute_script("arguments[0].scrollIntoView();", element)
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         return element.text
 
     def set_text_to_element(self, locator, text):
         element = self.find_element_with_wait(locator)
-        self.driver.execute_script("arguments[0].scrollIntoView();", element)
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         element.send_keys(text)
-
-    def click_java(self, locator):
-        js = "arguments[0].click();"
-        button = self.find_element_with_wait(locator)
-        self.driver.execute_script("arguments[0].click();", button)
 
     def switch_modal(self, locator):
         modal = self.find_element_with_wait(locator)
